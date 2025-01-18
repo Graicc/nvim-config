33d911391e78c25bc1e41f65d41764c026d8f6d3
Graicc <33105645+Graicc@users.noreply.github.com.>
2025-01-17 19:15:34 -0500

    Fix terminal config based on OS

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,18 +1,10 @@
 -- powershell
-if vim.fn.has('win32') then
+if vim.loop.os_uname().sysname == "Windows_NT" then
 	vim.opt.shell = 'pwsh'
 	vim.opt.shellcmdflag =
 	"-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
 	vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
 	vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
-	vim.opt.shellquote = ""
-	vim.opt.shellxquote = ""
-end
-if vim.fn.has('linux') then
-	vim.opt.shell = 'zsh'
-	vim.opt.shellcmdflag = "-c"
-	vim.opt.shellredir = ""
-	vim.opt.shellpipe = ""
 	vim.opt.shellquote = ""
 	vim.opt.shellxquote = ""
 end
